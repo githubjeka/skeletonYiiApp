718ae46b254e198347479224ee4f3dd666c86b2d
Evgeniy Tkachenko <et.coder@gmail.com>
2013-02-12 22:08:39 +0300

    update

--- protected/common/modules/install/views/install/step3.php
+++ protected/common/modules/install/views/install/step3.php
@@ -1,15 +1,15 @@
-<h1>Установка завершена</h1>
+<h1><?php echo Yii::t('install','Installation complete')?></h1>
 <div class="progress-bar">
     <div class="bar bg-color-pink" style="width: 30%"></div>
     <div class="bar bg-color-yellow" style="width: 30%"></div>
     <div class="bar bg-color-green" style="width: 40%"></div>
 </div>
 <div class="body-text">
-    <p>Для быстрого ознокомления возможностей CMS используйте следующие данные администратора </p>
-    <p>(Мы настоятельно рекомендуем их поменять.): </p>
-    <p class="indent">Логин по умолчанию: <strong>admin</strong></p>
-    <p class="indent">Пароль по умолчанию: <strong>adminсms</strong></p>
+    <p><?php echo Yii::t('install','For quick start use these administrator credentials')?></p>
+    <p><?php echo Yii::t('install','Strongly recommended to change them')?></p>
+    <p class="indent"><?php echo Yii::t('install','Username'); ?>: <strong>admin</strong></p>
+    <p class="indent"><?php echo Yii::t('install','Password'); ?>: <strong>adminсms</strong></p>
 </div>
 <?php
-echo CHtml::link('Войти',Yii::app()->createUrl('/users'));
+echo CHtml::link(Yii::t('install','Sign in'),Yii::app()->createUrl('/users'));
 ?>
